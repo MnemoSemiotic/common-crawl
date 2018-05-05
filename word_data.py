0cdd6c4a1088cc3ffed0f212f78e4fe28dd51ad3
Steven Silver <silver.steven@gmail.com>
2018-05-04 22:10:57 -0600

    word_data updated

--- word_data.py
+++ word_data.py
@@ -4,33 +4,41 @@ from pyspark.ml.feature import CountVectorizer
 from pyspark.ml.clustering import LDA
 from pyspark.ml.feature import StopWordsRemover
 from nltk.corpus import stopwords
+from pyspark.ml import Pipeline
+
+def generate_pipeline(lda_k=5,vocab=5000,min_token=4):
+    tokenizer = RegexTokenizer(inputCol='val', outputCol='tokens', pattern='\\W',minTokenLength=min_token)
+    sw = list(stopwords.words(['english','french','spanish']))
+    swr = StopWordsRemover(inputCol='tokens',outputCol='stop',stopWords=sw)
+    cv = CountVectorizer(inputCol='stop',vocabSize=vocab,minDF=10,outputCol='features')
+    lda = LDA(k=lda_k,optimizer='em')
+    pipeline = Pipeline(stages=[tokenizer,swr,cv,lda])
+    return pipeline
+
+def get_topic_words(pipeline):
+    topic = pipeline.stages[-1].describeTopics()
+    topic.registerTempTable('topic')
+    vocab = pipeline.stages[-2].vocabulary
+    topic_dict=dict()
+    for x in spark.sql('select topic,termIndices from topics').collect():
+        for y in x['termIndices']:
+            topic_dict.setdefault(x['topic'],[]).append(vocab[y])
+    return topic_dict
 
 if __name__ == '__main__':
     spark = ps.sql.SparkSession.builder.master("local")\
                                     .appName("case-study")\
                                     .getOrCreate()
 
-    word = spark.read.csv('corpus.csv')
-    word = word.selectExpr('_c0 as doc','_c1 as text')
-
+    word = spark.read.parquet('spark-warehouse/test/')
     word.registerTempTable('word')
-    word_trunc = spark.sql('''
-                            SELECT text
-                            FROM word
-                            WHERE text is not null
-                            ''')
 
-    tokenizer = RegexTokenizer(inputCol='text', outputCol='tokens', pattern='\\W',minTokenLength=4)
-    token = tokenizer.transform(word_trunc)
-    sw = list(stopwords.words(['english','french','spanish']))
-    swr = StopWordsRemover(inputCol='tokens',outputCol='stop',stopWords=sw)
-    word_sw = swr.transform(token)
-    word_sw.show(1)
-    cv = CountVectorizer(inputCol='stop',vocabSize=5000,minDF=10,outputCol='features')
-    cv_model = cv.fit(word_sw)
-    word_cv = cv_model.transform(word_sw)
+    word_trimmed = spark.sql('''SELECT *
+                                FROM test
+                                WHERE size(split(val,",")) > 10
+                                ''')
 
-    lda = LDA(k=5,optimizer='em')
+    pipeline = generate_pipeline()
+    pipeline.fit(word_trimmed)
 
-    lda_model = lda.fit(word_cv)
-    lda_model.describeTopics().show()
+    # lda_model.describeTopics().show()
